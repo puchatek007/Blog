297ebf4437d9e5cc276f643405fc7c7d8fdd1fdf
puchatek007 <jjpotempa@gmail.com>
2019-01-10 22:23:04 +0100

    koniec #1

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -6,7 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}                      #pole slug automatycznie jest wypełniane tytułem
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
