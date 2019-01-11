54f4b361b9dd3a04dc4e70dd9d4a40db117c2004
puchatek007 <jjpotempa@gmail.com>
2019-01-11 13:51:51 +0100

    dodanie tresci

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -13,9 +19,16 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'), self.slug])
+
